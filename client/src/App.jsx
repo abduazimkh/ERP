aa4644df69fedff43984b8f089d62a8097e3abcf
abduazimkh <azimjon8253@gmail.com>
2023-12-01 22:15:05 +0500

    app js added

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import RouteController from "./routes";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function App() {
+  return (
+    <>
+      <RouteController/>
+      <ToastContainer/>
+    </>
+  );
+}
+
+export default App;
